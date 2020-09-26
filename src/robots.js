0cc6b9d03cc070400de7c5894679bd3a065a3f07
Angira Tripathi <Angira@MBP-Developer.home>
2020-09-26 11:51:53 +0200

    altered css

---
+++ src/robots.js
@@ -0,0 +1,69 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Harry Potter',
+        username: '@hotter',
+        email: 'seeyou@warzone.com'
+    },
+    {
+        id: 2,
+        name: 'Draco Malfoy',
+        username: '@drafoy',
+        email: 'keepou@warzone.com'
+    },
+    {
+        id: 3,
+        name: 'Ron Weasley',
+        username: '@rosley',
+        email: 'rosleyu@warzone.com'
+    },
+    {
+        id: 4,
+        name: 'Dolores Umbridge',
+        username: '@doloymbridge',
+        email: 'dolo@warzone.com'
+    },
+    {
+        id: 5,
+        name: 'Regulus Black',
+        username: '@regulusblack',
+        email: 'rblack@warzone.com'
+    },
+    {
+        id: 6,
+        name: 'Serius Black',
+        username: '@seriusblack',
+        email: 'sblack@warzone.com'
+    },
+    {
+        id: 7,
+        name: 'Dobby',
+        username: '@dobby',
+        email: 'dob@warzone.com'
+    },
+    {
+        id: 8,
+        name: ' Lord Voldemort',
+        username: '@lovo',
+        email: 'lovo@warzone.com'
+    },
+    {
+        id: 9,
+        name: 'Rubeus Hagrid',
+        username: '@rubrid',
+        email: 'rubrid@warzone.com'
+    },
+    {
+        id: 10,
+        name: 'Hedwig',
+        username: '@hedwig',
+        email: 'hedwig@warzone.com'
+    },
+    {
+        id: 11,
+        name: ' Jacob Kowalski',
+        username: '@sJako',
+        email: 'jako@warzone.com'
+    },
+
+]
